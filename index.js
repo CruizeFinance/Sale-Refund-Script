4561c9f94ab31382ce970b6496e0e5cb54924d0d
salmanbao <salmancodez@gmail.com>
2023-04-29 17:36:09 +0500

    airdrop esCruize

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const ethers = require("ethers");
 const users = require("./users.json")
 const token_abi = require("./ERC20.json")
 const multicall_abi = require("./MULTICALL.json")
+require('dotenv').config();
+const fs = require("fs")
 
 const rpcUrl = process.env.RPC_URL;
 const usdc = process.env.USDC;
@@ -14,35 +16,43 @@ let wallet = new ethers.Wallet(privateKey,provider);
 let token = new ethers.Contract( usdc , token_abi , wallet );
 let multiCall = new ethers.Contract( multicall , multicall_abi , wallet );
 let total = 0;
+const priceOfCruize = 50000; // 0.05 * 10^6
+
 ( async ()=>{
 
-    let calls = [];
-    const initialValue = 0;
-    const sumWithInitial = users.reduce(
-        (accumulator, currentValue) => accumulator + parseInt(currentValue.amount),
-        initialValue
-        );
+    let transactions = [];
     
-    console.log("sumWithInitial:",sumWithInitial);
-    await token.approve(multicall,sumWithInitial)
-    const encodedForMulticall = token.interface.encodeFunctionData("transferFrom",[wallet.address,multicall,sumWithInitial]);
-
-    calls.push({
-        target:usdc,
-        callData:encodedForMulticall
-    });
-
     for (let index = 0; index < users.length; index++) {
         const element = users[index];
         total += parseInt(element.amount);
-        const encoded = token.interface.encodeFunctionData("transfer",[element.id,element.amount]);
-        calls.push({
-            target:usdc,
-            callData:encoded
-        });
-        console.log(`[${index+1}]: Amount of ${ethers.formatUnits(element.amount,6)} USDC has been airdroped to ${element.id}`);
-    }
-    let response = await multiCall.aggregate(calls);
-    await response.wait();
+        const rewardsInUSD = parseInt(element.amount) * 0.1;
+        const rewards = ethers.parseUnits((rewardsInUSD / priceOfCruize).toFixed(18),18);
+        transactions.push(
+            {
+                "to": "0x1f5229F1e1B96ca6189A7a0BFb04249757977bB2",
+                "value": "0",
+                "data": null,
+                "contractMethod": {
+                  "inputs": [
+                    { "name": "_to", "type": "address" },
+                    { "name": "_value", "type": "uint256" }
+                  ],
+                  "name": "transfer",
+                  "payable": false
+                },
+                "contractInputsValues": {
+                  "_to": element.id,
+                  "_value": rewards.toString()
+                }
+              }
+        )
 
+        var json = JSON.stringify(transactions);
+        fs.writeFile('tx.json', json,  function(err) {
+            if (err) throw err;
+            console.log('complete');
+            });
+
+
+    }
 })();
